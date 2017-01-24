b0abffde3ea305f1db710f950816c2f704415bfc
James Dudley <jedudley@outlook.com>
2017-01-24 16:17:57 -0600

    Added Truncate()

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -86,4 +86,30 @@ internal static class ExtensionMethods
         Array.Reverse(charArray);
         return new string(charArray);
     }
+	
+	/// <summary>Truncate a string to a maximum length.</summary>
+	/// <param name="str">The string to truncate.</param>
+	/// <param name="maxLength">The maximum length of the string after truncation.</param>
+	/// <param name="trailingAnnotationText">Trailing text annotation (if any) to indicate that the string has been truncated (e.g. "...").</param>
+	/// <returns>The truncated string.</returns>
+	public static string Truncate(this string str, int maxLength, string trailingAnnotationText = null)
+	{
+		if (str.Length > maxLength)
+		{
+			if (string.IsNullOrEmpty(trailingAnnotationText))
+			{
+				str = str.Substring(0, maxLength);
+			}
+			else if (str.Length <= trailingAnnotationText.Length)
+			{
+				str = trailingAnnotationText.Substring(0, maxLength);
+			}
+			else
+			{
+				str = str.Substring(0, maxLength - trailingAnnotationText.Length) + trailingAnnotationText;
+			}
+		}
+
+		return str;
+	}
 }
